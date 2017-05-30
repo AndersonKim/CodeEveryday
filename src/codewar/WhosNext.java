0085b27c3dc5f404648324fc232a5e260d0f4335
AndersonKim <pgytao@outlook.com>
2017-05-30 23:20:22 +0800

    beta version

--- src/codewar/WhosNext.java
+++ src/codewar/WhosNext.java
@@ -16,22 +16,47 @@ public class WhosNext {
         System.out.println(w.WhoIsNext(names, 2));
     }
 
-    public String WhoIsNext(String[] names, int n) {
-        String result = null;
-        ArrayList<String> round = new ArrayList<String>();
-        //set init data
-        for (String name : names) {
-            round.add(name);
+    public int xn(int counter) {
+        int y = 1;
+        for (int i = 0; i < counter; ++i) {
+            y = y * 2;
         }
-        //1.remove first
-        //2.add double first to tail
-        //3.till tail equals to n
-        if (round.size() > n) {
-            result = round.get(n - 1);
+        return y;
+    }
+
+    public int sum(int counter) {
+        int m = 5, ma = 1;
+        for (int i = 0; i < counter; i++) {
+            ma = ma * 2;
+            m = m + 5 * ma;
+        }
+        return m;
+    }
+
+    public int getIndex(int n) {
+        int counter;
+        int x = 5, y = 1, res = 0;
+        if (n < 6) {
+            res = n;
         } else {
+            counter = 0;
+            while (n > sum(counter)) {
+                counter++;
+            }
+            counter = counter - 1;
+            n = n - sum(counter);
+            res = (n - 1) / xn(counter + 1) + 1;
 
         }
+        if (res == 1) return 1;
+        if (res == 2) return 2;
+        if (res == 3) return 3;
+        if (res == 4) return 4;
+        if (res == 5) return 5;
+        return 0;
+    }
 
-        return result;
+    public String WhoIsNext(String[] names, int n) {
+        return names[getIndex(n)];
     }
 }
