428894295d875ca1a7e3679b3da14cf5b5e7b345
AndersonKim <pgytao@outlook.com>
2017-06-01 17:20:08 +0800

    matrix sum solution

--- src/LintCode/P170601.java
+++ src/LintCode/P170601.java
@@ -12,7 +12,15 @@ package LintCode;
  * return [1, 2]
  */
 public class P170601 {
-
+    public int sumOfElements(int matrix[][]) {
+        int res = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                res += matrix[i][j];
+            }
+        }
+        return res;
+    }
     public int[] twoSum(int[] nums, int target) {
         //标注检索的最大位置
         int max_pos = nums.length - 1;
--- src/Test.java
+++ src/Test.java
@@ -9,10 +9,7 @@ import LintCode.P170601;
 public class Test {
     public static void main(String[] args) {
         P170601 p170601 = new P170601();
-        int[] num = {-1, 0, 1};
-        int[] res = p170601.twoSum(num, -1);
-        for (int i : res) {
-            System.out.println(i);
-        }
+        int[][] a = {{1, 0}, {0, 1}, {1, 2}};
+        System.out.println(p170601.sumOfElements(a));
     }
 }
