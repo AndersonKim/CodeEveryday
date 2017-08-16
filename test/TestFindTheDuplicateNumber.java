4f8fada28f9dec6df7d98eeb318df71bf737c238
Digger <pgytao@outlook.com>
2017-08-16 17:25:36 +0800

    restructure FindTheDuplicateNumber problem sloving

--- test/TestFindTheDuplicateNumber.java
+++ test/TestFindTheDuplicateNumber.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class TestFindTheDuplicateNumber {
     int[] testcase1 = {8, 4, 6, 2, 6, 4, 7, 9, 5, 8};
     int[] testcase2 = {2, 3, 3, 1, 5, 2};
+    int[] testcase3 = {2, 2, 1, 1};
 
     @Test
     public void testFirstDuplicate() {
