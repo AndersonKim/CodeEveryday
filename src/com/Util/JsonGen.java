27b7b3ed39aa600955ebd5848402f353020fc248
Digger <pgytao@outlook.com>
2017-08-21 11:26:56 +0800

    build a simple data Generator for ztree

---
+++ src/com/Util/JsonGen.java
@@ -0,0 +1,72 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+//gen simple data for ztree
+public class JsonGen {
+    public String genJsonStr() {
+        JsonNode a = new JsonNode("A", true, true);
+        a.children = new ArrayList<JsonNode>();
+        a.children.add(new JsonNode("A-1", false, false));
+        a.children.add(new JsonNode("A-2", false, false));
+        a.children.add(new JsonNode("A-3", false, false));
+        a.children.add(new JsonNode("A-4", false, false));
+        a.children.add(new JsonNode("A-5", false, false));
+        a.children.add(new JsonNode("A-6", false, false));
+        a.children.add(new JsonNode("A-7", false, false));
+        a.children.add(new JsonNode("A-8", false, false));
+        return a.toString();
+
+
+    }
+
+    @Test
+    public void test() {
+        JsonGen a = new JsonGen();
+        System.out.println(a.genJsonStr());
+    }
+}
+
+class JsonNode {
+    public String name;
+    public Boolean open;
+    public Boolean parent;
+    public ArrayList<JsonNode> children;
+
+    public JsonNode(String name, Boolean open, Boolean parent) {
+        this.name = name;
+        this.open = open;
+        this.parent = parent;
+    }
+
+    public JsonNode() {
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        //using \" represent " in java String
+        sb.append("{name:\"" + name + "\", isParent:" + parent + ",open:" + open);
+
+        if (children.size() > 0) {
+            sb.append(",children:");
+            //formating children string
+            StringBuffer childStr = new StringBuffer();
+            for (JsonNode child : children) {
+                childStr.append("{name:\"" + child.name + "\"},");
+            }
+            childStr.insert(0, "[");
+            int lastQutoPos = childStr.lastIndexOf(",");
+            childStr.deleteCharAt(lastQutoPos);
+            childStr.append("]");
+            sb.append(childStr);
+        }
+        sb.append("}");
+
+
+        return sb.toString();
+    }
+
+
+}
--- test/TestFindTheDuplicateNumber.java
+++ test/TestFindTheDuplicateNumber.java
@@ -1,14 +1,12 @@
-import org.junit.Test;
-
 public class TestFindTheDuplicateNumber {
     int[] testcase1 = {8, 4, 6, 2, 6, 4, 7, 9, 5, 8};
     int[] testcase2 = {2, 3, 3, 1, 5, 2};
     int[] testcase3 = {2, 2, 1, 1};
     int[] testcase4 = {1, 1, 2, 2, 1};
 
-    @Test
+
     public void testFirstDuplicate() {
-        FindTheDuplicateNumber ffdn = new FindTheDuplicateNumber();
-        ffdn.firstDuplicateAI(testcase4);
+      /*  FindTheDuplicateNumber ffdn = new FindTheDuplicateNumber();
+        ffdn.firstDuplicateAI(testcase4);*/
     }
 }
