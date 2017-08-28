c241183d0a6528ad8f353cdc050882c6199f1460
Digger <pgytao@outlook.com>
2017-08-28 14:39:26 +0800

    -update add sum in 3 solution

--- src/com/LintCode/ThreeSum.java
+++ src/com/LintCode/ThreeSum.java
@@ -1,5 +1,8 @@
 package LintCode;
 
+import java.util.ArrayList;
+
+//todo unfinished codes
 public class ThreeSum {
     /*
     Given an array S of n integers,
@@ -15,6 +18,31 @@ public class ThreeSum {
     */
 
     public int threeSumClosest(int[] num, int target) {
+        int result = Integer.MAX_VALUE;
+        return result;
+    }
 
+
+    /*
+    Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+    Find all unique triplets in the array which gives the sum of zero.
+    Example
+    For example, given array S = {-1 0 1 2 -1 -4}, A solution set is:
+    (-1, 0, 1)
+    (-1, -1, 2)
+    Note
+    Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+    The solution set must not contain duplicate triplets.
+    Tags Expand
+    Two Pointers Sort Array Facebook
+    */
+    public ArrayList<ArrayList<Integer>> threeSum(int[] numbers) {
+        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
+        if (numbers == null && numbers.length <= 2) {// Length at least >= 3
+            return rst;
+        }
+
+
+        return rst;
     }
 }
