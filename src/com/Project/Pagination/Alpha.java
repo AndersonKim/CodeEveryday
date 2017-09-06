227023dd2827ae3045e77b63f7114d8d41818714
AndersomKim <pgytao@outlook.com>
2017-09-06 21:42:13 +0800

    -using JDBC to manipulate the mysql database

---
+++ src/com/Project/Pagination/Alpha.java
@@ -0,0 +1,50 @@
+package com.Project.Pagination;
+
+import com.Util.JDBCUtil;
+import org.junit.Test;
+
+import java.sql.*;
+
+/**
+ * edited by AndersonKim
+ * at 2017/9/6
+ */
+public class Alpha {
+    /**
+     * 使用如下方法实现分页：
+     * 1.获取当前数据表的行数
+     * 2.根据分页的条件对总体数据进行处理
+     */
+    Connection connection = null;
+    ResultSet resultSet = null;
+    PreparedStatement preparedStatement = null;
+    Statement statement = null;
+
+    /**
+     * 获取当前表的行数
+     * @return
+     */
+    public int getRowNum(){
+        int result=-1;
+       connection=JDBCUtil.getMySQLConn();
+        try {
+
+            statement=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            String sql="select * from sfzinfo";
+            resultSet=statement.executeQuery(sql);
+            resultSet.last();
+            result=resultSet.getRow();//获得ResultSet的总行数
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            JDBCUtil.closeConn(resultSet,statement,connection);
+        }
+        return result;
+    }
+
+
+    @Test
+    public void get(){
+        System.out.println("======================================"+getRowNum());
+    }
+}
